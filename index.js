600eae80d88e0c8836ed3d53973d4d6b564adb05
Sadid-Hasan <sadid.hasan94@gmail.com>
2023-10-24 10:33:58 +0600

    Database connecion with client side dome

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const carCollection =client.db('carDB').collection('car');
+
+
+
+
+    app.post('/car',async(req,res)=>{
+      const newProduct =req.body;
+      console.log(newProduct);
+      const result =await carCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
